619177543e69c92bb6c5b6b00a981e297fd32a63
Roman-Khair <roman27040437@gmail.com>
2022-11-02 20:56:30 +0500

    Greet White

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -4,6 +4,11 @@ namespace HelloApp
 {
     internal class Program
     {
+        public static void GreetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("hello");
